b4e048e929ee8dd7a32c07feabf61e2e6bfe54b7
mosheFuks <moshifuks2001@gmail.com>
2025-09-13 22:46:28 -0300

    Comm-MAJ

--- src/components/users/VisitorUserComponents/AniversaryComponents/AniversaryModal.tsx
+++ src/components/users/VisitorUserComponents/AniversaryComponents/AniversaryModal.tsx
@@ -175,6 +175,7 @@ export const CreateAniversaryModalComponent = ({modalAniversaryIsOpen, setModalA
             }} style={styles.input}>
               <option value="" disabled selected>{formUserAniversaryData.motivo != "" ? formUserAniversaryData.motivo : 'Selecciona el motivo'}</option>
               <option value="Yortzait">Yortzait</option>
+              <option value="Jupa">Jupa</option>
               <option value="Otro">Otro</option>
             </select>
           )}
